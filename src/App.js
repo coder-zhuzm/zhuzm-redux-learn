8ec304f6511cfb0f1e645ab6223496c056018171
coder-zhuzm <zhuzm1916@163.com>
2022-01-15 22:39:24 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Redux from "./Redux";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Redux />
+    </div>
+  );
+}
---
+++ src/Redux/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * author coder-zhuzm
+ * date 2022-01-15 22:30
+ */
+
+import React from "react";
+
+export default () => {
+  return <div>hello redux</div>;
+};
